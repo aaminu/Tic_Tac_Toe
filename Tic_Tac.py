fd7e0ae6244e1d2d98b9c6196e09d9fab2ea1b9e
Abdulrahman Aminu <24601677+aaminu@users.noreply.github.com>
2020-04-18 12:39:44 +0200

    Game script

---
+++ Tic_Tac.py
@@ -0,0 +1,17 @@
+import random
+import time
+
+
+def tic_tac_toe():
+    pass
+
+
+def main():
+    print(' Welcome to the Xs and Os, aka TicTacToe')
+    time.sleep(2)
+    print('Loading....')
+    tic_tac_toe()
+
+
+if __name__ == '__main__':
+    main()
